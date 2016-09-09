80437ee024cff30c68dcfcd5f62277935b540d9b
gonzalomz <gonzalom312@gmail.com>
2016-09-09 01:19:33 -0300

    correccion boluda

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ if('production' == env){
 
 // Routes
 
-app.route('/'). get(routes.index);
+app.route('/').get(routes.index);
 
 app.listen(app.get('port'), function(){
   console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
