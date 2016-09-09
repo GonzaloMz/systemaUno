bdd3a93c9ccf79a97ae093d1e1d5c86d6bf8e339
gonzalomz <gonzalom312@gmail.com>
2016-09-08 21:40:30 -0300

    mejorado en los comentarios

--- IC/IntegrarDevelop.sh
+++ IC/IntegrarDevelop.sh
@@ -1,10 +1,11 @@
 
 
 #Actualizar develop
-
 git checkout develop
 git pull --rebase
 
-#Ejecutar pruebas
+
+
+#Ejecutar pruebas (RunAllTest realiza la tarea de iniciar el server para las pruebas)
 cd ../QA/
 ./RunAllTest.sh > ../IC/integration_log.txt
